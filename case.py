9edc6768013db9a912e0dd6d01c5654df50f03ef
Egor <geo-04@mail.ru>
2023-11-27 20:58:46 +0700

    1

--- case.py
+++ case.py
@@ -1,71 +1,71 @@
-from turtle import *
+import turtle
 
 def branch_3(n, size):
     if n == 0:
-        left(180)
+        turtle.left(180)
         return
 
     x = size/(n+1)
     for i in range(n):
-        forward(x)
-        left(45)
+        turtle.forward(x)
+        turtle.left(45)
         branch_3(n-i-1, 0.5*x*(n-i-1))
-        left(90)
+        turtle.left(90)
         branch_3(n-i-1, 0.5*x*(n-i-1))
-        right(135)
+        turtle.right(135)
 
-    forward(x)
-    left(180)
-    forward(size)
+    turtle.forward(x)
+    turtle.left(180)
+    turtle.forward(size)
 
 def minkovsi_6(n, size):
     if n == 0:
-        forward(size)
+        turtle.forward(size)
         return
 
     else:
         minkovsi_6(n-1, size/2)
-        left(90)
+        turtle.left(90)
         minkovsi_6(n-1,size/2)
-        right(90)
+        turtle.right(90)
         minkovsi_6(n-1,size/2)
-        right(90)
+        turtle.right(90)
         minkovsi_6(n-1, size)
-        left(90)
+        turtle.left(90)
         minkovsi_6(n-1,size/2)
-        left(90)
+        turtle.left(90)
         minkovsi_6(n-1,size/2)
-        right(90)
+        turtle.right(90)
         minkovsi_6(n-1,size/2)
 
 def ice_fractal_7(n, size):
     if n == 0:
-        forward(size)
+        turtle.forward(size)
         return
 
     else:
         ice_fractal_7(n - 1,size)
-        left(90)
+        turtle.left(90)
         ice_fractal_7(n - 1, size / 2)
-        right(180)
+        turtle.right(180)
         ice_fractal_7(n - 1, size / 2)
-        left(90)
+        turtle.left(90)
         ice_fractal_7(n - 1, size)
 
 def eg_10(n, size):
     if n == 0:
-        forward(size)
+        turtle.forward(size)
         return
 
     else:
         eg_10(n - 1,size / 1.5)
-        left(135)
+        turtle.left(135)
         eg_10(n - 1,size / 4)
-        right(135)
+        turtle.right(135)
         eg_10(n - 1, (size/4) * 2 ** (1/2))
-        right(135)
+        turtle.right(135)
         eg_10(n - 1, size / 4)
-        left(135)
+        turtle.left(135)
         eg_10(n - 1, size / 1.5)
 
 
@@ -81,29 +81,29 @@ size = int(input('Длина стороны: '))
 
 
 if fractal == '1':
-    up()
-    goto(-400,-100)
-    left(90)
-    down()
+    turtle.up()
+    turtle.goto(-400,-100)
+    turtle.left(90)
+    turtle.down()
     branch_3(n,size)
 
 if fractal == '2':
-    up()
-    goto(-400, 0)
-    down()
+    turtle.up()
+    turtle.goto(-400, 0)
+    turtle.down()
     minkovsi_6(n,size)
 
 if fractal == '3':
-    up()
-    goto(-400, 0)
-    down()
+    turtle.up()
+    turtle.goto(-400, 0)
+    turtle.down()
     ice_fractal_7(n,size)
 
 if fractal == '4':
-    up()
-    goto(-400, 0)
-    down()
+    turtle.up()
+    turtle.goto(-400, 0)
+    turtle.down()
     eg_10(n,size)
 
 
-done()
+turtle.done()
